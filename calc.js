6e4deff00b76cb3da0e675d4aff7c67c8a6f05a5
ZZPapillon <ipapillondeveloper@gmail.com>
2022-09-07 20:36:03 +0200

    skoro gotovo

--- calc.js
+++ calc.js
@@ -1,134 +1,77 @@
 const container = document.querySelector('.container');
 const display = document.querySelector('.input');
+const backspace = document.querySelector('.backspace');
+const numbers = document.querySelectorAll('.number');
 
-const one = document.querySelector('.one');
-const two = document.querySelector('.two');
-const three = document.querySelector('.three');
-const four = document.querySelector('.four');
-const five = document.querySelector('.five');
-const six = document.querySelector('.six');
-const seven = document.querySelector('.seven');
-const eight = document.querySelector('.eight');
-const nine = document.querySelector('.nine');
-const zero = document.querySelector('.zero');
-
-const add = document.querySelector('.add');
-const subtract = document.querySelector('.subtract');
-const mulitiply = document.querySelector('.mutliply');
-const divide = document.querySelector('.divide');
+const operators = document.querySelectorAll('.operator');
 
 const equal = document.querySelector('.equal');
 const clear = document.querySelector('.clear');
-
-let a = 0;
-let b = 0;
-let operator = '';
 const p = document.createElement('div');
-p.classList.add('p');
-function add1(a,b) {
-    return a + b;
-}
-function substract1(a, b) {
-    return a - b;
-}
-function mutliply1(a, b) {
-    return (a * b);
-}
-function divide1(a, b) {
-    return (a / b);
-}
-function operate(a, b, operator) {
-    switch (operator) {
-        case "+":
-            return add1(a, b);
-        case "-":
-            return subtract1(a, b);
-        case "*":
-            return multiply1(a, b);
-        case "/":
-            return divide1(a, b);
+          p.classList.add('p');
+          display.appendChild(p);
+
+let previousValue = '';  //previous value
+let currentValue = '';   // current value
+let operator = '';
+
+numbers.forEach((number) => number.addEventListener('click', function(e) {
+    handleNumber(e.target.textContent);
+    p.textContent = currentValue;
+    console.log(number);
+}))
+
+function handleNumber(num) {
+    if (currentValue.length < 6) {
+    currentValue += num;
     }
 }
+operators.forEach((op) => op.addEventListener('click', function (e) {
+    handleOperator(e.target.textContent);
+    p.textContent = previousValue + '' + operator;
+}))
+function handleOperator(op) {
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
+}
+
+equal.addEventListener('click',   () => {
+    calculate();
+    p.textContent = previousValue;
+})
+
+function calculate() {
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+    
+      if(operator === '+') {
+      previousValue = previousValue + currentValue;
+    } else if(operator === '-') {
+    previousValue = previousValue - currentValue;
+        } else if(operator === 'x') {
+     previousValue = previousValue * currentValue;
+        } else {
+      previousValue = previousValue / currentValue;
+      }
+        previousValue = roundNumber(previousValue);
+        previousValue = previousValue.toString();   
+         currentValue = previousValue.toString();
+    }
 
 
-one.addEventListener('click', () => {  
-  p.textContent = p + 1;
-  display.appendChild(p);
-  
-});
+function roundNumber(num) {
+    return Math.round(num * 1000) / 1000;
+}
 
-two.addEventListener('click', () => {
-    p.textContent = 2;
-    display.appendChild(p);
-    b = 2;
-});
 
-one.addEventListener('click', () => {
-    p.textContent = 1;
-    display.appendChild(p);
-});
+clear.addEventListener('click', function() {
+    previousValue = '';
+    currentValue = '';
+    p.textContent = currentValue;
+    operator = '';
+})
 
-three.addEventListener('click', () => {
-    p.textContent = 3;
-    display.appendChild(p);
-});
-
-four.addEventListener('click', () => {
-    p.textContent = 4;
-    display.appendChild(p);
-});
-
-five.addEventListener('click', () => {
-    p.textContent = 5;
-    display.appendChild(p);
-});
-
-six.addEventListener('click', () => {
-    p.textContent = 6;
-    display.appendChild(p);
-});
-
-seven.addEventListener('click', () => {
-    p.textContent = 7;
-    display.appendChild(p);
-});
-
-eight.addEventListener('click', () => {
-    p.textContent = 8;
-    display.appendChild(p);
-});
-
-nine.addEventListener('click', () => {
-    p.textContent = 9;
-    display.appendChild(p);
-});
-
-add.addEventListener('click', () => {
-    operator = "+";
-   // p.textContent = operate(a, b, operator)
-   // display.appendChild(p);
-});
-
-subtract.addEventListener('click', () => {
-    operator = '-';
-    //p.textContent = operate(a, b, operator)
-    //display.appendChild(p);
-});
-
-mulitiply.addEventListener('click', () => {
-    operator = '*';
-   // p.textContent = operate(a, b, operator)
-   // display.appendChild(p);
-});
-
-divide.addEventListener('click', () => {
-    operator = '/';
-   // p.textContent = operate(a, b, operator)
-   // display.appendChild(p);
-});
-
-equal.addEventListener('click', () => {
-    p.textContent = operate(a, b, operator);
-    display.appendChild(p);
-});
-
+backspace.addEventListener('click', function () {
+    curremtValue = currentValue.slice(0, -1);
+})
